4537b40078e2d661506d1df00764b78ce041ea3d
mp2626 <71911671+mp2626@users.noreply.github.com>
2021-03-20 21:36:55 +1100

    restart, added seperate html and updated js

--- assets/script.js
+++ assets/script.js
@@ -1,47 +1,66 @@
 // vars for game
 var timer = document.querySelector("#timer")
-var startClock = document.querySelector(".startTimerQuiz")
+var startQuiz = document.querySelector(".startQuiz")
+var questionScreen = document.querySelector("#questions")
+
+
+
+var timerClock = ""
+var questionIndex = 0
+var gameOver = false
+
+
 var questionArray = [
     {
-        question: "What is the meaning of API",
-        choices: ["Asset Program Interface", "Application Program Interference", "Application Programming Interface"]
-
+        question: "What is the meaning of API?",
+        choices: ["Asset Program Interface", "Application Programing Interference", "Application Programming Interface",],
+        answer: "Application Programming Interface"
     },
+    {
+        question: "In order to call a function once defined, you should use which of the following?",
+        choices: ["[]", "()", "{}",],
+        answer: "()"
+    }
 ]
-
-
-var timerClock = 2
 
 // timer function / check for win lose
 
-function startTimer() {
-    startClock.style.display = "none";
+function startGame() {
+    startQuiz.style.display = "none";
+    timerClock = 10;
+    startTimer()
+}
 
+function startTimer() {
     clock = setInterval(function () {
         if (timerClock > 0) {
             timerClock--;
             timer.textContent = timerClock;
         }
-        if (timerClock <= 0) {
-            return
+        else {
+            startQuiz.style.display = "";
+            clearInterval(clock)
         }
     }, 1000)
+
+    renderQuestions()
 }
-
-
 
 // fetch and render questions
 
-function quizQuestions() {
-
-
+function renderQuestions() {
+    questionScreen.textContent = questionArray[questionIndex].question;
+    questionIndex++;
 }
 
+// render options and remove for next question https://www.w3schools.com/jsref/met_element_remove.asp
+
+// compare questions event and reset index?
 
 // add to score/remove
 
 
-// score reset
+// score reset and quiz reset
 
 
 // high score add name to ul
@@ -50,5 +69,5 @@ function quizQuestions() {
 
 // event listener 
 
-startClock.addEventListener("click", startTimer)
+startQuiz.addEventListener("click", startGame)
 
